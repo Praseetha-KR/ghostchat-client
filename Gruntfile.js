25fa42332a2027087c80b500cda77f5b299fd408
Sharat M R <cosmosgenius@gmail.com>
2014-11-17 21:01:52 +0530

    removed coffee

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,7 +62,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-coffee');
 
     grunt.registerTask('default', ['less:development', 'connect', 'watch']);
     grunt.registerTask('test', []);
